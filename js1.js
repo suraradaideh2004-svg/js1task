fbbdd344b0d1a9d302cc0bfaa66570dbe2653b7d
suraradaideh2004-svg <suraradaideh2004@gmail.com>
2025-09-24 11:50:19 +0300

    js1-2

---
+++ js1.js
@@ -0,0 +1,74 @@
+// ################### TASK 1 ###################
+
+let cash = 1000;
+let currentLiabilities = 500;
+
+let cashFlowRatio = cash / currentLiabilities;
+console.log(cashFlowRatio);
+
+// ################### TASK 2 ###################
+
+let revenues = 1000;
+let expenses = 500;
+
+console .log (revenues-expenses); 
+
+// ################### TASK 3 ###################
+let liabilities = 1000;
+let equity = 500;
+console .log (liabilities + equity);
+
+// ################### TASK 4 ###################
+let profit= 1000;
+let sales= 500;
+console .log ( profit * sales);
+
+// ################### TASK 5 ###################
+let n1=7;
+let n2=9;
+let n3=2;
+let sum = n1+n2+n3;
+let avg= sum /3;
+console.log (avg);
+
+  // ################### TASK 6 ###################
+let price =150;
+let discount=30
+console .log ( price-(discount/100 ) *150);
+
+
+// ################### TASK 7 ###################
+let age =20;
+console .log (18<age && age <30);
+
+
+// ################### TASK 8 ###################
+let num1=2;
+let num2=3;
+console .log (num1**num2);
+// ################### TASK 9 ###################
+let numb1 =10;
+let numb2 =4;
+console .log (num1 % num2);
+//task1-2
+console. log (typeof "100");
+console .log ( typeof 73.4);
+console .log ( typeof NaN);
+console .log ( typeof "water");
+console .log ( typeof false)
+console .log ( typeof (9!=11));
+console .log (" orange"+"e");
+console .log ( "orange"-"s");
+console .log ( "4"+"8");
+console .log ( "4"-"8");
+console .log ( "name"+3);
+console .log ( "name"-3);
+console .log ( 82*"name");
+console .log ( 1+"name");
+console .log ( "name"+3);
+console .log ( 1+true);
+console .log ( "name"+true);
+console .log ( typeof (Infinity));
+console .log ( 1==`1`);
+console .log ( 1=== `1`);
+
